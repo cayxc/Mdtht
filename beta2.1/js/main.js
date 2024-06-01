85debfe09ca525115c23c5e1e06d555bfd2f619a
CAYXC <cayxc512@613.com>
2024-06-01 21:05:33 +0800

    样式问题修改

--- beta2.1/js/main.js
+++ beta2.1/js/main.js
@@ -1,15 +1,16 @@
-// 查询系统是否使用了暗色主题
-const osThemeIsDark = matchMedia("(prefers-color-scheme: dark)").matches;
+// 查询系统是否使用了dark主题
+let osThemeIsDark = matchMedia("(prefers-color-scheme: dark)").matches;
 let htmlElement = document.querySelector('html');
-if(osThemeIsDark){
+
+//系统开启了，但手动没有开启dark主题，没有自定义
+if(osThemeIsDark && htmlElement.getAttribute('theme') != 'dark'){
   htmlElement.setAttribute("theme","dark");
-}else{
-  htmlElement.setAttribute("theme","");
 }
+
 //监听系统主题变化
 matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
-  // htmlElement.dataset.theme = event.matches ? 'dark' : '';
-  let theme = event.matches ? 'dark': '';
-  htmlElement.setAttribute('theme', theme);
+  //event.matches: dark => true
+  // console.log(event.matches);
+  event.matches ? htmlElement.setAttribute("theme","dark") : htmlElement.removeAttribute("theme");
 });
 
